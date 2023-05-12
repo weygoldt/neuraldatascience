e9aa2739bda983a9994ead5718b15233c5cc4517
wendtalexander <wendtalexander@protonmail.com>
2023-05-12 22:05:08 +0200

    first exercise

---
+++ notebooks/CodingLab4.py
@@ -0,0 +1,536 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# _Neural Data Science_
+# 
+# Lecturer: Prof. Dr. Philipp Berens
+# 
+# Tutors: Jonas Beck, Ziwei Huang, Rita González Márquez
+# 
+# Summer term 2023
+# 
+# Name: FILL IN YOUR NAMES HERE
+# 
+# # Coding Lab 4
+# 
+# If needed, download the data files ```nds_cl_4_*.csv``` from ILIAS and save it in the subfolder ```../data/```. Use a subset of the data for testing and debugging, ideally focus on a single cell (e.g. cell number x). The spike times and stimulus conditions are read in as pandas data frames. You can solve the exercise by making heavy use of that, allowing for many quite compact computationis. If you need help on that, there is lots of [documentation](http://pandas.pydata.org/pandas-docs/stable/index.html) and several good [tutorials](https://www.datacamp.com/community/tutorials/pandas-tutorial-dataframe-python#gs.L37i87A) are available online. Of course, converting the data into classical numpy arrays is also valid.
+
+# In[1]:
+
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import scipy.optimize as opt
+from IPython import embed
+from scipy import signal as signal
+
+import itertools
+
+# get_ipython().run_line_magic('matplotlib', 'inline')
+
+# get_ipython().run_line_magic('load_ext', 'jupyter_black')
+
+# get_ipython().run_line_magic('load_ext', 'watermark')
+# get_ipython().run_line_magic('watermark', '--time --date --timezone --updated --python --iversions --watermark -p sklearn')
+
+
+# In[2]:
+
+
+plt.style.use("../matplotlib_style.txt")
+
+
+# ## Load data
+
+# In[3]:
+
+
+spikes = pd.read_csv("../data/nds_cl_4_spiketimes.csv")  # neuron id, spike time
+stims = pd.read_csv("../data/nds_cl_4_stimulus.csv")  # stimulus onset in ms, direction
+
+stimDur = 2000.0  # in ms
+nTrials = 11  # number of trials per condition
+nDirs = 16  # number of conditions
+deltaDir = 22.5  # difference between conditions
+
+stims["StimOffset"] = stims["StimOnset"] + stimDur
+
+
+# We require some more information about the spikes for the plots and analyses we intend to make later. With a solution based on dataframes, it is natural to compute this information here and add it as additional columns to the `spikes` dataframe by combining it with the `stims` dataframe. We later need to know which condition (`Dir`) and trial (`Trial`) a spike was recorded in, the relative spike times compared to stimulus onset of the stimulus it was recorded in (`relTime`) and whether a spike was during the stimulation period (`stimPeriod`). But there are many options how to solve this exercise and you are free to choose any of them.
+
+# In[4]:
+
+
+# you may add computations as specified above
+spikes["Dir"] = np.nan
+spikes["relTime"] = np.nan
+spikes["Trial"] = np.nan
+spikes["stimPeriod"] = np.nan
+
+dirs = np.unique(stims["Dir"])
+trialcounter = np.zeros_like(dirs)
+
+for i, row in stims.iterrows():
+    trialcounter[dirs == row["Dir"]] += 1
+
+    i0 = spikes["SpikeTimes"] > row["StimOnset"]
+    i1 = spikes["SpikeTimes"] < row["StimOffset"]
+
+    select = i0.values & i1.values
+
+    spikes.loc[select, "Dir"] = row["Dir"]
+    spikes.loc[select, "Trial"] = trialcounter[dirs == row["Dir"]][0]
+    spikes.loc[select, "relTime"] = spikes.loc[select, "SpikeTimes"] - row["StimOnset"]
+    spikes.loc[select, "stimPeriod"] = True
+
+spikes = spikes.dropna()
+
+
+# In[8]:
+
+
+spikes.head()
+
+
+# ## Task 1: Plot spike rasters
+# 
+# In a raster plot, each spike is shown by a small tick at the time it occurs relative to stimulus onset. Implement a function `plotRaster()` that plots the spikes of one cell as one trial per row, sorted by conditions (similar to what you saw in the lecture). Why are there no spikes in some conditions and many in others?
+# 
+# If you opt for a solution without a dataframe, you need to change the interface of the function.
+# 
+# *Grading: 2 pts*
+# 
+
+# In[29]:
+
+
+def plotRaster(spikes, neuron):
+    """plot spike rasters for a single neuron sorted by condition
+
+    Parameters
+    ----------
+
+    spikes: pd.DataFrame
+        Pandas DataFrame with columns
+            Neuron | SpikeTimes | Dir | relTime | Trial | stimPeriod
+
+    neuron: int
+        Neuron ID
+
+
+    Note
+    ----
+
+    this function does not return anything, it just creates a plot!
+    """
+
+    fig, ax = plt.subplots(figsize=(8, 4))
+  
+    # get subset of the dataframe for the neuron
+    spikes_neuron = spikes[spikes["Neuron"] == neuron]
+
+    # sort the spikes by direction
+    spikes_neuron = spikes_neuron.sort_values(by="Dir")
+    # plot the spikes for each trial in a raster plot
+
+
+    for trial in np.unique(spikes_neuron["Trial"]):
+        spikes_trial = spikes_neuron[spikes_neuron["Trial"] == trial]
+        ax.scatter(
+            spikes_trial["SpikeTimes"],
+            spikes_trial["Trial"],
+            marker="|",
+            color="k",
+            s=10,
+            linewidths=0.5,
+        )
+
+    # -------------------------------------------------
+    # Write a raster plot function for the data (2 pts)
+    # -------------------------------------------------
+
+    # insert your code here
+    # stim direction should be on the y-axis and time on the x-axis
+    # you can use plt.scatter or plt.plot to plot the responses to each stim
+
+
+# Show examples of different neurons. Good candidates to check are 28, 29, 36 or 37. 
+
+# In[30]:
+
+
+plotRaster(spikes, 28)
+embed()
+exit()
+plotRaster(spikes, 29)
+plotRaster(spikes, 36)
+plotRaster(spikes, 37)
+
+
+# ## Task 2: Plot spike density functions
+# 
+# Compute an estimate of the spike rate against time relative to stimulus onset. There are two ways:
+# * Discretize time: Decide on a bin size, count the spikes in each bin and average across trials. 
+# * Directly estimate the probability of spiking using a density estimator with specified kernel width. 
+# 
+# Implement one of them in the function `plotPsth()`. If you use a dataframe you may need to change the interface of the function.
+# 
+# 
+# *Grading: 2 pts*
+# 
+
+# In[ ]:
+
+
+def plotPSTH(spikes, neuron):
+    """Plot PSTH for a single neuron sorted by condition
+
+    Parameters
+    ----------
+
+    spikes: pd.DataFrame
+        Pandas DataFrame with columns
+            Neuron | SpikeTimes | Dir | relTime | Trial | stimPeriod
+
+    neuron: int
+        Neuron ID
+
+
+    Note
+    ----
+
+    this function does not return anything, it just creates a plot!
+    """
+
+    # insert your code here
+
+    fig, ax = plt.subplots(figsize=(8, 4))
+
+    # ------------------------------------------------
+    # Implement one of the spike rate estimates (1 pt)
+    # ------------------------------------------------
+
+    # ---------------------------------------------
+    # Plot the obtained spike rate estimates (1 pt)
+    # ---------------------------------------------
+
+    # plot should look similar to `plotRaster`
+    # you can plot use plt.hist for each direction, but much cleaner
+    # is to only plot bin centers vs bin heights using plt.plot
+
+
+# Show examples of different neurons. Good candidates to check are 28, 29, 36 or 37. 
+
+# In[ ]:
+
+
+plotPSTH(spikes, 28)
+plotPSTH(spikes, 29)
+plotPSTH(spikes, 36)
+plotPSTH(spikes, 37)
+
+
+# In[ ]:
+
+
+plotRaster(spikes, 37)
+plotPSTH(spikes, 37)
+
+
+# ## Task 3: Fit and plot tuning functions
+# 
+# The goal is to visualize the activity of each neuron as a function of stimulus direction. First, compute the spike counts of each neuron for each direction of motion and trial.  The result should be a matrix `x`, where $x_{jk}$ represents the spike count of the $j$-th response to the $k$-th direction of motion (i.e. each column contains the spike counts for all trials with one direction of motion).	If you used dataframes above, the `groupby()` function allows to implement this very compactely. Make sure you don't loose trials with zero spikes though. Again, other implementations are completely fine.
+# 
+# Fit the tuning curve, i.e. the average spike count per direction, using a von Mises model. To capture the non-linearity and direction selectivity of the neurons, we will fit a modified von Mises function:
+# 
+# $$ f(\theta) = \exp(\alpha + \kappa (\cos (2*(\theta-\phi))-1) + \nu (\cos (\theta-\phi)-1))$$
+# 
+# Here, $\theta$ is the stimulus direction. Implement the von Mises function in `vonMises()` and plot it to understand how to interpret its parameters $\phi$, $\kappa$, $\nu$, $\alpha$. Perform a non-linear least squares fit using a package/function of your choice. Implement the fitting in `tuningCurve()`. 
+# 
+# Plot the average number of spikes per direction, the spike counts from individual trials as well as your optimal fit.
+# 
+# Select two cells that show nice tuning to test you code.
+# 
+# *Grading: 3 pts*
+
+# In[ ]:
+
+
+def vonMises(θ, α, κ, ν, ϕ):
+    """Evaluate the parametric von Mises tuning curve with parameters p at locations theta.
+
+    Parameters
+    ----------
+
+    θ: np.array, shape=(N, )
+        Locations. The input unit is degree.
+
+    α, κ, ν, ϕ : float
+        Function parameters
+
+    Return
+    ------
+    f: np.array, shape=(N, )
+        Tuning curve.
+    """
+
+    # insert your code here
+
+    # -----------------------------------
+    # Implement the Mises model (0.5 pts)
+    # -----------------------------------
+
+    return f
+
+
+# Plot the von Mises function while varying the parameters systematically.
+
+# In[ ]:
+
+
+# --------------------------------------------------------------------------------
+# plot von Mises curves with varying parameters and explain what they do (0.5 pts)
+# --------------------------------------------------------------------------------
+
+fig, ax = plt.subplots(figsize=(8, 4))
+
+
+# In[ ]:
+
+
+def tuningCurve(counts, dirs, show=True):
+    """Fit a von Mises tuning curve to the spike counts in count with direction dir using a least-squares fit.
+
+    Parameters
+    ----------
+
+    counts: np.array, shape=(total_n_trials, )
+        the spike count during the stimulation period
+
+    dirs: np.array, shape=(total_n_trials, )
+        the stimulus direction in degrees
+
+    show: bool, default=True
+        Plot or not.
+
+
+    Return
+    ------
+    p: np.array or list, (4,)
+        parameter vector of tuning curve function
+    """
+
+    # insert your code here
+
+    # ----------------------------------------
+    # Compute the spike count matrix (0.5 pts)
+    # ----------------------------------------
+
+    # ------------------------------------------------------------
+    # fit the von Mises tuning curve to the spike counts (0.5 pts)
+    # ------------------------------------------------------------
+
+    if show == True:
+        # -----------------------------------------------
+        # plot the data and fitted tuning curve (0.5 pts)
+        # -----------------------------------------------
+
+        fig, ax = plt.subplots(figsize=(7, 5))
+
+        # the plot should contain both the data and the fitted curve
+        # using seaborn makes this really easy
+
+        return
+    else:
+        return p
+
+
+# Plot tuning curve and fit for different neurons. Good candidates to check are 28, 29 or 37. 
+
+# In[ ]:
+
+
+def get_data(spikes, neuron):
+    spk_by_dir = (
+        spikes[spikes["Neuron"] == neuron]
+        .groupby(["Dir", "Trial"])["stimPeriod"]
+        .sum()
+        .astype(int)
+        .reset_index()
+    )
+
+    dirs = spk_by_dir["Dir"].values
+    counts = spk_by_dir["stimPeriod"].values
+
+    # because we count spikes only when they are present, some zero entries in the count vector are missing
+    for i, Dir in enumerate(np.unique(spikes["Dir"])):
+        m = nTrials - np.sum(dirs == Dir)
+        if m > 0:
+            dirs = np.concatenate((dirs, np.ones(m) * Dir))
+            counts = np.concatenate((counts, np.zeros(m)))
+
+    idx = np.argsort(dirs)
+    dirs_sorted = dirs[idx]  # sorted dirs
+    counts_sorted = counts[idx]
+
+    return dirs_sorted, counts_sorted
+
+
+# In[ ]:
+
+
+# ---------------------------------------------------------
+# plot tuning curve and fit for different neurons (0.5 pts)
+# ---------------------------------------------------------
+
+dirs, counts = get_data(spikes, 28)
+# add plot
+
+
+# In[ ]:
+
+
+dirs, counts = get_data(spikes, 29)
+# add plot
+
+
+# In[ ]:
+
+
+dirs, counts = get_data(spikes, 36)
+# add plot
+
+
+# In[ ]:
+
+
+dirs, counts = get_data(spikes, 37)
+# add plot
+
+
+# In[ ]:
+
+
+dirs, counts = get_data(spikes, 32)
+# add plot
+
+
+# ## Task 4: Permutation test for direction tuning
+# 
+# Implement a permutation test to quantitatively assess whether a neuron is direction/orientation selective. To do so, project the vector of average spike counts, $m_k=\frac{1}{N}\sum_j x_{jk}$ on a complex exponential with two cycles, $v_k = \exp(\psi i \theta_k)$, where $\theta_k$ is the $k$-th direction of motion in radians and $\psi \in 1,2$ is the fourier component to test (1: direction, 2: orientation). Denote the projection by $q=m^Tv$. The magnitude $|q|$ tells you how much power there is in the $\psi$-th fourier component. 
+# 
+# Estimate the distribution of |q| under the null hypothesis that the neuron fires randomly across directions by running 1000 iterations where you repeat the same calculation as above but on a random permutation of the trials (that is, randomly shuffle the entries in the spike count matrix x). The fraction of iterations for which you obtain a value more extreme than what you observed in the data is your p-value. Implement this procedure in the function ```testTuning()```. 
+# 
+# Illustrate the test procedure for one of the cells from above. Plot the sampling distribution of |q| and indicate the value observed in the real data in your plot. 
+# 
+# How many cells are tuned at p < 0.01?
+# 
+# *Grading: 3 pts*
+# 
+
+# In[ ]:
+
+
+def testTuning(counts, dirs, psi=1, niters=1000, show=False):
+    """Plot the data if show is True, otherwise just return the fit.
+
+    Parameters
+    ----------
+
+    counts: np.array, shape=(total_n_trials, )
+        the spike count during the stimulation period
+
+    dirs: np.array, shape=(total_n_trials, )
+        the stimulus direction in degrees
+
+    psi: int
+        fourier component to test (1 = direction, 2 = orientation)
+
+    niters: int
+        Number of iterations / permutation
+
+    show: bool
+        Plot or not.
+
+    Returns
+    -------
+    p: float
+        p-value
+    q: float
+        magnitude of second Fourier component
+
+    qdistr: np.array
+        sampling distribution of |q| under the null hypothesis
+
+    """
+
+    # insert your code here
+
+    # -------------------------------
+    # calculate m, nu and q (0.5 pts)
+    # -------------------------------
+
+    # -------------------------------------------------------------------------
+    # Estimate the distribution of q under the H0 and obtain the p value (1 pt)
+    # -------------------------------------------------------------------------
+
+    if show == True:
+        # -------------------------------
+        # plot the test results (0.5 pts)
+        # -------------------------------
+        fig, ax = plt.subplots(figsize=(7, 4))
+        # you can use sns.histplot for the histogram
+    else:
+        return p, q, qdistr
+
+
+# Show null distribution for the example cell:
+
+# In[ ]:
+
+
+# ------------------------------------------------------------
+# Plot null distributions for example cells 28 & 29. (0.5 pts)
+# ------------------------------------------------------------
+
+dirs, counts = get_data(spikes, 28)
+# add plot
+
+
+# In[ ]:
+
+
+dirs, counts = get_data(spikes, 29)
+# add plot
+
+
+# Test all cells for orientation and direction tuning
+
+# In[ ]:
+
+
+# -------------------------------------------------------
+# Test cells for orientation / direction tuning (0.5 pts)
+# -------------------------------------------------------
+
+# collect p values for orientation / direction selectivity
+
+
+# Number of direction tuned neurons:
+
+# In[ ]:
+
+
+# count cells with p > 0.01 (which ones are they?)
+
+
+# Number of orientation tuned neurons:
+
+# In[ ]:
+
+
+# count cells with p > 0.01 (which ones are they?)
+
