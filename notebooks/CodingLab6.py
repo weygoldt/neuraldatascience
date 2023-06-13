220290f49a2e21067346f8a9f162732b8574eb89
wendtalexander <wendtalexander@protonmail.com>
2023-06-13 20:24:22 +0200

    trying to finish the excersise

--- notebooks/CodingLab6.py
+++ notebooks/CodingLab6.py
@@ -2,28 +2,28 @@
 # coding: utf-8
 
 # _Neural Data Science_
-#
+# 
 # Lecturer: Prof. Dr. Philipp Berens
-#
+# 
 # Tutors: Jonas Beck, Ziwei Huang, Rita González Márquez
-#
+# 
 # Summer term 2023
-#
-# Names: FILL IN YOUR NAMES HERE
-#
+# 
+# Names: Kathrin Root, Alexander Wendt, Patrick Weygoldt
+# 
 # # Coding Lab 6
-#
+# 
 # In this exercise we are going to fit a latent variable model (Poisson GPFA) to both toy data and real data from monkey primary visual cortex.
 
 # ## Preliminaries
-#
-# ### 1. Code
-#
-# The toolbox we are going to use contains an implementation of the EM algorithm to fit the poisson-gpfa.
-#
+# 
+# ### 1. Code 
+# 
+# The toolbox we are going to use contains an implementation of the EM algorithm to fit the poisson-gpfa. 
+# 
 # Assuming you `git clone https://github.com/mackelab/poisson-gpfa` to the notebooks/ directory and have the following directory structure:
-#
-#
+# 
+# 
 # ```
 # ├── data/
 # │   └── nds_cl_6_data.mat
@@ -33,30 +33,30 @@
 # ├── matplotlib_style.txt
 # ├── requirements.txt
 # ```
-#
+# 
 # then you can import the related functions via:
-#
+# 
 # ```
 # import sys
 # sys.path.append('./poisson-gpfa/')
 # sys.path.append('./poisson-gpfa/funs')
-#
+# 
 # import funs.util as util
 # import funs.engine as engine
 # ```
-#
+# 
 # Change the paths if you have different directory structure. For the details of the algorithm, please refer to the thesis `hooram_thesis.pdf` from ILIAS.
-#
+# 
 # ### 2. Data
-#
+# 
 # Download the data file ```nds_cl_6_data.mat``` from ILIAS and save it in a ```data/``` folder.
 
-# In[1]:
+# In[3]:
 
 
 import numpy as np
 import scipy.io as sio
-import matplotlib.pyplot as plt  #
+import matplotlib.pyplot as plt
 from IPython import embed
 
 # style
@@ -79,33 +79,27 @@ import funs.engine as engine
 # get_ipython().run_line_magic('watermark', '--time --date --timezone --updated --python --iversions --watermark -p sklearn')
 
 
-# In[2]:
+# In[4]:
 
 
 plt.style.use("../matplotlib_style.txt")
 
 
 # ## Task 1. Generate some toy data to test the poisson-GPFA code
-#
+# 
 # We start by verifying our code on toy data. The cell below contains code to generate data for 30 neurons, 100 trials (1000 ms each) and 50ms bin size. The neurons' firing rate $\lambda_k$ is assumed to be a constant $d_k$ modulated by a one-dimensional latent state $x$, which is drawn from a Gaussian process:
-#
+# 
 # $\lambda_k = \exp(c_kx + d_k)$
-#
+# 
 # Each neuron's weight $c_k$ is drawn randomly from a normal distribution and spike counts are sampled form a Poisson distribution with rate $\lambda_k$.
-#
+# 
 # Your task is to fit a Poisson GPFA model with one latent variable to this data (see `engine.PPGPFAfit`).
-#
+# 
 # Hint: You can use `util.dataset?`, `engine.PPGPFAfit?` or `util.initializeParams?` to find out more about the provided package.
-#
+# 
 # *Grading: 3 pts*
 
-# In[3]:
-
-
-# get_ipython().run_line_magic('pinfo', 'util.dataset')
-
-
-# In[18]:
+# In[5]:
 
 
 # ---------------------------------
@@ -127,24 +121,11 @@ trainigs_dataset = util.dataset(
     xdim=latent_variabel,
     seed=seed,
 )
-trainigs_dataset.plotTrajectory()
-
-
-# In[5]:
-
-
-# get_ipython().run_line_magic('pinfo', 'util.initializeParams')
-
-
-# # In[6]:
-
-
-# get_ipython().run_line_magic('pinfo', 'engine.PPGPFAfit')
 
 
 # ### Fit the model
 
-# In[7]:
+# In[6]:
 
 
 # -----------------------
@@ -155,13 +136,12 @@ trainigs_dataset.plotTrajectory()
 params = util.initializeParams(
     xdim=latent_variabel, ydim=neurons, experiment=trainigs_dataset
 )
-print(params.keys())
 
 # choose sensible parameters and run fit
 fitToy = engine.PPGPFAfit(trainigs_dataset, params, batchSize=100)
 
 
-# In[8]:
+# In[7]:
 
 
 # some useful functions
@@ -186,18 +166,9 @@ def allTrialsX(training_set):
 # Verify your fit by plotting both ground truth and inferred parameters for:
 # 1. weights C
 # 2. biases d
-# 3. latent state x
-#
+# 3. latent state x 
+# 
 # Note that the sign of fitted latent state and its weights are ambiguous (you can flip both without changing the model). Make sure you correct the sign for the plot if it does not match the ground truth.
-
-# In[9]:
-
-
-# All trials latent state vector
-x_est = allTrialsState(fitToy, 1)
-x_true = allTrialsX(trainigs_dataset)
-print(x_est.shape, x_true.shape)
-
 
 # In[21]:
 
@@ -325,7 +296,7 @@ ax.legend(loc=(0.8, 0.99))
 # consider also plotting the optimal weights as a dotted line for reference
 
 
-# In[12]:
+# In[17]:
 
 
 # ------------------------------------------------------
@@ -333,125 +304,92 @@ ax.legend(loc=(0.8, 0.99))
 # ------------------------------------------------------
 
 trials = 100
-first_trial = 40
-last_trial = 50
+first_trial = 45
+last_trial = 47
+dt = 1000  # ms for each trial
+bin_size = 50  # ms
 plot_trials = np.arange(first_trial, last_trial)
 print(np.shape(fitToy.infRes["post_mean"]))
 
 fig, ax = plt.subplots(figsize=(12, 3))
-
-start = first_trial * 20
-end = start + 20
-padding = 5
-start_and_stops = []
-
+embed()
+exit()
 for i in plot_trials:
-    if i == plot_trials[0]:
-        start_and_stops.extend([start, end - 1])
+    time_trial = np.arange(i * dt, (i + 1) * dt, bin_size)
+    # because the sign of the latend states are arbitrary we need to flip them for trial 48
+    if i == 48:
         ax.plot(
-            np.arange(start, end),
+            time_trial,
             trainigs_dataset.data[i]["X"][0],
             color="red",
             linewidth=3,
             label="Ground truth",
         )
         ax.plot(
-            np.arange(start, end),
+            time_trial,
             fitToy.infRes["post_mean"][i][0],
             color="blue",
             linewidth=3,
+        )
+        ax.axvline(time_trial[-1], ymin=-2, ymax=3, color="gray", alpha=0.8)
+        # trial number in on the top of the plot
+        ax.text(
+            time_trial / 2 - 7,
+            3,
+            f"Trial {i}",
+            verticalalignment="center",
+            fontsize=12,
             label="Inferred",
         )
-        ax.axvspan(end - 1, end + padding, color="gray", alpha=0.8)
-        # trial number in the middle of the plot
-        ax.text(
-            (start + end) / 2 - 10,
-            3,
-            f"Trial {i}",
-            verticalalignment="center",
-            fontsize=12,
-        )
-        start += 20
-        end += 20
-    elif i == 48:
-        start_and_stops.extend([start + padding, end - 1 + padding])
-        ax.plot(
-            np.arange(start + padding, end + padding),
-            trainigs_dataset.data[i]["X"][0],
-            color="red",
-            linewidth=3,
-        )
-        ax.plot(
-            np.arange(start + padding, end + padding),
-            fitToy.infRes["post_mean"][i][0],
-            color="blue",
-            linewidth=3,
-        )
-        ax.axvspan(end - 1 + padding, end + padding + padding, color="gray", alpha=0.8)
-        # trial number in on the top of the plot
-        ax.text(
-            (start + padding + end + padding) / 2 - 7,
-            3,
-            f"Trial {i}",
-            verticalalignment="center",
-            fontsize=12,
-        )
-        start = end + padding
-        end += 20 + padding
-    else:
-        start_and_stops.extend([start + padding, end - 1 + padding])
-        ax.plot(
-            np.arange(start + padding, end + padding),
-            trainigs_dataset.data[i]["X"][0],
-            color="red",
-            linewidth=3,
-        )
-        ax.plot(
-            np.arange(start + padding, end + padding),
-            -fitToy.infRes["post_mean"][i][0],
-            color="blue",
-            linewidth=3,
-        )
-        ax.axvspan(end - 1 + padding, end + padding + padding, color="gray", alpha=0.8)
-        # trial number in on the top of the plot
-        ax.text(
-            (start + padding + end + padding) / 2 - 7,
-            3,
-            f"Trial {i}",
-            verticalalignment="center",
-            fontsize=12,
-        )
-        start = end + padding
-        end += 20 + padding
+    ax.plot(
+        time_trial,
+        trainigs_dataset.data[i]["X"][0],
+        color="red",
+        linewidth=3,
+        label="Ground truth",
+    )
+    ax.plot(
+        time_trial,
+        -fitToy.infRes["post_mean"][i][0],
+        color="blue",
+        linewidth=3,
+        label="Inferred",
+    )
+    ax.axvline(time_trial[-1], ymin=-2, ymax=3, color="gray", alpha=0.8)
+    # trial number in on the top of the plot
+    ax.text(
+        time_trial / 2 - 7,
+        3,
+        f"Trial {i}",
+        verticalalignment="center",
+        fontsize=12,
+        label="Inferred",
+    )
+
+
+    # if i == plot_trials[0]:
+    #     ax.legend(loc=(0.9, 1.01))
+
 
 ax.set_xlabel("Time [ms]")
 ax.set_ylabel("Latent state")
-ax.legend(loc=(0.9, 1.01))
 ax.set_title("Latent states", pad=20)
-ax.set_xticks(
-    start_and_stops,
-)
-ax.set_xticklabels(start_and_stops, rotation=45)
-
+# ax.set_xticks(np.arange(first_trial * dt, (last_trial + 1) * dt + 1, bin_size))
 
 # add plot
 # plot only for a subset of trials
 # consider seperating each trial by a vertical line
 
 
-# ## Task 2: Fit GPFA model to real data.
-#
-# We now fit the model to real data and cross-validate over the dimensionality of the latent variable.
-#
-# *Grading: 2 pts*
-#
-#
+# ## Task 2: Fit GPFA model to real data. 
+# 
+# We now fit the model to real data and cross-validate ov
 
 # ### Load data
-#
+# 
 # The cell below implements loading the data and encapsulates it into a class that matches the interface of the Poisson GPFA engine. You don't need to do anything here.
 
-# In[13]:
+# In[140]:
 
 
 class EckerDataset:
@@ -494,9 +432,12 @@ class EckerDataset:
         self.binSize = binSize
         self.numTrials = numTrials
         self.ydim = ydim
+        util.dataset.getMeanAndVariance(self)
+        util.dataset.getAvgFiringRate(self)
+        util.dataset.getAllRaster(self)
 
 
-# In[14]:
+# In[146]:
 
 
 path = "../data/nds_cl_6_data.mat"
@@ -504,21 +445,21 @@ data = EckerDataset(path)
 
 
 # ### Fit Poisson GPFA models and perform model comparison
-#
+# 
 # Split the data into 80 trials used for training and 20 trials held out for performing model comparison. On the training set, fit models using one to five latent variables. Compute the performance of each model on the held-out test set.
-#
+# 
 # Hint: You can use the `crossValidation` function in the Poisson GPFA package.
-#
+# 
 # Optional: The `crossValidation` function computes the mean-squared error on the test set, which is not ideal. The predictive log-likelihood under the Poisson model would be a better measure, which you are welcome to compute instead.
 
 # ### Derivation for log-likelihood
-#
+# 
 # _You can add your calculations in_ $\LaTeX$ _here_.
-#
+# 
 # $p_\lambda(x_t) = \ldots$
-#
-# $L(\lambda_k; x_1, ..., x_N) = \ldots$
-#
+# 
+# $L(\lambda_k; x_1, ..., x_N) = \ldots$ 
+# 
 # $log(L) = l(\lambda_k; x_1, ..., x_N) = \ldots$
 
 # In[15]:
@@ -534,7 +475,7 @@ initParams = util.initializeParams(xdim, data.ydim, data)
 fitBatch = engine.PPGPFAfit(data, initParams, batchSize=data.numTrials)
 
 
-# In[30]:
+# In[160]:
 
 
 # do the actual cross validation
@@ -544,14 +485,16 @@ xval = util.crossValidation(
     numTrainingTrials=80,
     numTestTrials=20,
     maxXdim=maxXdim,
+    learningMethod="diag",
+    batchSize=80,
 )
 
 
 # ### Plot the test error
-#
+# 
 # Make a plot of the test error for the five different models. As a baseline, please also include the test error of a model without a latent variable. This is essentially the mean-squared error of a constant rate model (or Poisson likelihood if you did the optional part above).
 
-# In[27]:
+# In[159]:
 
 
 # -----------------------------------------------------------------------------------------
@@ -563,10 +506,10 @@ train_set, test_set = util.splitTrainingTestDataset(
 )
 
 # compute baseline error
-# baseline_error = np.mean((test_set - np.mean(train_set)) ** 2)
+baseline_error = np.mean((test_set - np.mean(train_set)) ** 2)
 
 
-# In[31]:
+# In[163]:
 
 
 # Your plot here
@@ -583,31 +526,16 @@ plt.title("Latent Dimension vs. Prediction Error")
 # ax.axhline(baseline_error, linestyle="--")
 
 
-# ## Task 3. Visualization: population rasters and latent state. Use the model with a single latent state.
-#
-# Create a raster plot where you show for each trial the spikes of all neurons as well as the trajectory of the latent state `x` (take care of the correct time axis). Sort the neurons by their weights `c_k`.
-#
-#
+# ## Task 3. Visualization: population rasters and latent state. Use the model with a single latent state. 
+# 
+# Create a raster plot where you show for each trial the spikes of all neurons as well as the trajectory of the latent state `x` (take care of the correct time axis). Sort the neurons by their weights `c_k`. 
+# 
+# 
 # Plot only the first 20 trials.
-#
+# 
 # *Grading: 2 pts*
 
-# In[95]:
-
-
-print(np.shape(fitBatch.infRes["post_mean"]))
-print(np.shape(data.data[3]["spike_time"][0]))
-c = fitBatch.paramSeq[-1]["C"][:, 0]
-# print(c)
-
-# # sort c by x
-# neurons = data.data[3]["spike_time"][30]
-# print(neurons)
-# # sorted_neurons = data.data[3]["spike_time"][x][3]
-# print(sorted_neurons)
-
-
-# In[111]:
+# In[164]:
 
 
 from numpy import matlib
@@ -626,8 +554,11 @@ xs = 0.7 * xa * np.sin(ts / 1000 * 3.4 * 2 * np.pi) + xa
 
 # sorted 'c'
 index_sorted_c = np.argsort(fitBatch.paramSeq[-1]["C"][:, 0])
+
+
 def squeezing(trial_data):
     return [np.squeeze(trial_data[i]) for i in range(len(trial_data))]
+
 
 with sns.axes_style("ticks"):
     for ntrial, ax in enumerate(axs.flat):
@@ -636,22 +567,40 @@ with sns.axes_style("ticks"):
         # ------------------------
         # plot latent state (1 pt)
         # ------------------------
-        ax.plot(x, fitBatch.infRes["post_mean"][ntrial][0])
+        # plot latent state on different y axis
+        ax2 = ax.twinx()
+        ax2.plot(x, fitBatch.infRes["post_mean"][ntrial][0])
+        ax2.set_yticks(np.arange(-5, 5))
+        ax2.set_yticklabels([])
+        ax2.set_yticks([])
 
         # hint: can be plotted on top of the corresponding raster
 
-        # sort neurons by firing rate
+        # sort neurons by weights
         neurons_trial = data.data[ntrial]["spike_time"][index_sorted_c]
         neruons_trial_squeezed = squeezing(neurons_trial)
         # ----------------------------------
         # plot raster for each neuron (1 pt)
         # ----------------------------------
-        for neruon in neruons_trial_squeezed:
-            ax.scatter(neruon, np.ones(len(neruon)) * 0.5, s=1, color="black")
+        for neuron in range(len(neruons_trial_squeezed)):
+            if neurons_trial[neuron].size == 1:
+                continue
+            if neurons_trial[neuron].size == 0:
+                continue
+
+            else:
+                ax.eventplot(
+                    neruons_trial_squeezed[neuron],
+                    lineoffsets=neuron,
+                    linelengths=0.6,
+                    linewidths=0.6,
+                    color="black",
+                )
+        ax.set_title("Trial " + str(ntrial + 1), loc="left")
 
         if ntrial == 0:
             ax.legend()
-        if ntrial == 1:
+        if ntrial == 19:
             ax.plot([1000, 2000], [-30, -30], color="green")
             ax.text(1300, -50, "1sec")
         if ntrial < 2:
@@ -659,21 +608,22 @@ with sns.axes_style("ticks"):
 
         ax.set_yticks([])
         ax.set_xticks([])
+        fig.supxlabel("Time [ms]")
 
 
 # ## Task 4. Visualization of covariance matrix.
-#
+# 
 # Plot (a) the covariance matrix of the observed data as well as its approximation using (b) one and (c) five latent variable(s). Use the analytical solution for the covariance matrix of the approximation*. Note that the solution is essentially the mean and covariance of the [log-normal distribution](https://en.wikipedia.org/wiki/Log-normal_distribution).
-#
+# 
 # $ \mu = \exp(\frac{1}{2} \text{ diag}(CC^T)+d)$
-#
-# $ \text{Cov}= \mu\mu^T \odot \exp(CC^T)+\text{ diag}(\mu) - \mu\mu^T$
-#
+# 
+# $ \text{Cov}= \mu\mu^T \odot \exp(CC^T)+\text{ diag}(\mu) - \mu\mu^T$ 
+# 
 # *[Krumin, M., and Shoham, S. (2009). Generation of Spike Trains with Controlled Auto- and Cross-Correlation Functions. Neural Computation 21, 1642–1664](http://www.mitpressjournals.org/doi/10.1162/neco.2009.08-08-847).
-#
+# 
 # *Grading: 3 pts*
 
-# In[ ]:
+# In[188]:
 
 
 # insert your code here
@@ -686,8 +636,22 @@ with sns.axes_style("ticks"):
 
 def cov(fit):
     # add your code here
+    mu = np.exp(
+        (
+            np.diag(fit.paramSeq[-1]["C"] @ fit.paramSeq[-1]["C"].T)
+            + fit.paramSeq[-1]["d"]
+        )
+        / 2
+    )
+    c = (
+        (mu @ mu.T) * np.exp(fit.paramSeq[-1]["C"] @ fit.paramSeq[-1]["C"].T)
+        + np.diag(mu)
+        - (mu @ mu.T)
+    )
     return c, mu
 
+
+print(np.shape(xval.fits[0].infRes["post_cov"]))
 
 # --------------------------------------------------------------
 # Plot the covariance matrix (1 pt) of
@@ -705,3 +669,15 @@ vmax = 1
 
 fig, axs = plt.subplots(1, 3, figsize=(10, 3.5))
 # add plot to visualize the differences in the covariance matrices
+
+axs[0].imshow(obs_corr, vmin=vmin, vmax=vmax, cmap="winter")
+axs[0].set_title("Observed Data")
+axs[1].imshow(opt_r1, vmin=vmin, vmax=vmax, cmap="winter")
+
+axs[1].set_title("1 Latent Variable")
+axs[2].imshow(opt_r5, vmin=vmin, vmax=vmax, cmap="winter")
+
+axs[2].set_title("5 Latent Variables")
+fig.supxlabel("Neuron Index")
+fig.supylabel("Neuron Index")
+
